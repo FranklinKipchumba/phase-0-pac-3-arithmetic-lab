31b695f2f90a92d59319b74a3260076546888090
FranklinKipchumba <sitieneifranklin@gmail.com>
2022-11-19 13:10:53 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,69 @@
+let a = 9;
+let b = 7;
+function beforeEach(){
+    let MA = Math.floor(Math.random(a)*1000)
+    return(MA);
+}
+function beforeEach(){
+    let MB = Math.floor(Math.random(b)*1000)
+    return(MB);
+}
+
+add();
+subtract();
+multiply();
+divide();
+
+function add(a,b){
+    let sum = a + b;
+    return(sum);
+}
+function subtract(a,b){
+    let difference = a - b;
+    return(difference);
+}
+function multiply(a,b){
+    let product = a * b;
+    return(product);
+}
+function divide(a,b){ 
+    let division = a / b;
+    return(division);
+}
+
+function increment(a){
+    let n = a + 1;
+    return(n);
+}
+function decrement(a){
+    let n = a - 1;
+    return(n);
+}
+function makeInt(a){
+    let m = a.toString();
+    return(m);
+}
+function log10(m){
+    let base = m;
+    console.log(math.log10(base));
+}
+function NaN(m){
+    let NaN = m >= 'sldkjflksjf';
+    return(NaN);
+}
+function preserveDecimal(m){
+    let p = m / 2.222 * 2.222;
+    return(p);
+}
+function NaN(p){
+    let NaN2 = p >= sldkjflksjf;
+    return(NaN2);
+}
+
+
+
+
+
+
+
+
